feeba654a74d07443dc4e2115953046dcf50275e
Enamulali <enamul97@icloud.com>
2024-12-10 16:10:30 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Travel Itinerary Creator</h1>
+      {/* Components will go here */}
+    </div>
+  );
+};
+
+export default App;
